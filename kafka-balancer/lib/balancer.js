f88705833b0922f18f116e83fe817ada75b03a47
Rafael Mota <rma7@cin.ufpe.br>
2020-11-17 15:00:10 -0300

    Refactor balancer logic

--- kafka-balancer/lib/balancer.js
+++ kafka-balancer/lib/balancer.js
@@ -1,34 +1,44 @@
 function balancePartitions(brokers, partitions) {
+    const totalPartitionQuantity = partitions.length;
+    const brokersWithAllocatedPartitions = groupPartitionsByBroker(brokers, partitions);
+
+    return generateOptimalPartitionAllocation(brokersWithAllocatedPartitions, totalPartitionQuantity);
+}
+
+function groupPartitionsByBroker(brokers, partitions) {
     const brokersWithAllocatedPartitions = brokers.map(({ nodeId }) => {
         const allocatedPartitions = partitions.filter(partition => partition.leader === nodeId)
 
         return { nodeId, partitions: allocatedPartitions }
     });
+
     brokersWithAllocatedPartitions.sort((a, b) => b.partitions.length - a.partitions.length);
 
-    const totalPartitionQuantity = partitions.length;
-    const expectedPartitionQuantity = Math.floor(totalPartitionQuantity / brokers.length)
+    return brokersWithAllocatedPartitions;
+}
+
+function generateOptimalPartitionAllocation(partitionsGroupedByBroker, totalPartitionQuantity) {
+    const expectedPartitionQuantity = Math.floor(totalPartitionQuantity / partitionsGroupedByBroker.length)
     let unallocatedPartitions = [];
 
-    const brokersWithNewAllocation = brokersWithAllocatedPartitions.map(broker => {
-        let newAllocatedPartitions = broker.partitions
-        let addedPartitions = [];
+    return partitionsGroupedByBroker.map(broker => {
         if (broker.partitions.length > expectedPartitionQuantity) {
-            newAllocatedPartitions = broker.partitions.slice(0, expectedPartitionQuantity);
+            // if the broker has more partitions than expected, add the spare partitions
+            // to unallocated partitions
+            unallocatedPartitions = unallocatedPartitions.concat(
+                broker.partitions.slice(expectedPartitionQuantity, broker.partitions.length)
+            );
 
-            unallocatedPartitions = unallocatedPartitions.concat(broker.partitions.slice(expectedPartitionQuantity, broker.partitions.length));
+            return { nodeId: broker.nodeId, addedPartitions: [] };
         } else if (broker.partitions.length < expectedPartitionQuantity) {
+            // if the broker has less partitions than expected, get the needed partitions
+            // from the unallocated partitions list
             const quantityNeeded = expectedPartitionQuantity - broker.partitions.length
-            addedPartitions = unallocatedPartitions.splice(0, quantityNeeded)
+            const addedPartitions = unallocatedPartitions.splice(0, quantityNeeded)
 
-            newAllocatedPartitions = broker.partitions.concat(addedPartitions)
+            return { nodeId: broker.nodeId, addedPartitions };
         }
-
-        return { nodeId: broker.nodeId, partitions: newAllocatedPartitions, addedPartitions: addedPartitions }
-    });
-
-
-    return brokersWithNewAllocation;
+   });
 }
 
 module.exports = {
